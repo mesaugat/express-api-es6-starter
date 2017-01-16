0b7a76ef1894bdba914c9abc8e670e8558ab0488
Saugat Acharya <mesaugat@gmail.com>
2017-01-17 00:46:07 +0545

    Add missing tests (UPDATE, DELETE) for users API

--- test/controllers/users.test.js
+++ test/controllers/users.test.js
@@ -3,10 +3,12 @@ import request from 'supertest';
 import app from '../../src/index';
 import bookshelf from '../../src/db';
 
+/**
+ * Tests for '/api/users'
+ */
 describe('Users Controller Test', () => {
-  beforeEach((done) => {
-    bookshelf.knex('users').del()
-      .then(() => done());
+  before((done) => {
+    bookshelf.knex('users').truncate().then(() => done());
   });
 
   it('should return list of users', (done) => {
@@ -16,6 +18,26 @@ describe('Users Controller Test', () => {
         expect(res.statusCode).to.be.equal(200);
         expect(res.body.data).to.be.an('array');
         expect(res.body.data).to.have.lengthOf(0);
+
+        done();
+      });
+  });
+
+  it('should not create a new user if name is not provided', (done) => {
+    let user = {
+      noname: 'Jane Doe'
+    };
+
+    request(app)
+      .post('/api/users')
+      .send(user)
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(400);
+        expect(res.body.error.code).to.be.equal(400);
+        expect(res.body.error.message).to.be.equal('Bad Request');
+        expect(res.body.error.details).to.be.an('array');
+        expect(res.body.error.details[0]).to.have.property('message');
+        expect(res.body.error.details[0]).to.have.property('param', 'name');
 
         done();
       });
@@ -41,13 +63,33 @@ describe('Users Controller Test', () => {
       });
   });
 
-  it('should not create a new user if name is not provided', (done) => {
+  it('should update a user if name is provided', (done) => {
     let user = {
-      noname: 'Jane Doe'
+      name: 'John Doe'
     };
 
     request(app)
-      .post('/api/users')
+      .put('/api/users/1')
+      .send(user)
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data).to.have.property('id');
+        expect(res.body.data).to.have.property('name');
+        expect(res.body.data).to.have.property('updated_at');
+        expect(res.body.data.name).to.be.equal(user.name);
+
+        done();
+      });
+  });
+
+  it('should not update a user if name is not provided', (done) => {
+    let user = {
+      noname: 'John Doe'
+    };
+
+    request(app)
+      .put('/api/users/1')
       .send(user)
       .end((err, res) => {
         expect(res.statusCode).to.be.equal(400);
@@ -56,6 +98,28 @@ describe('Users Controller Test', () => {
         expect(res.body.error.details).to.be.an('array');
         expect(res.body.error.details[0]).to.have.property('message');
         expect(res.body.error.details[0]).to.have.property('param', 'name');
+
+        done();
+      });
+  });
+
+  it('should delete a user if valid id is provided', (done) => {
+    request(app)
+      .delete('/api/users/1')
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(204);
+
+        done();
+      });
+  });
+
+  it('should respond with not found error if random user id is provided for deletion', (done) => {
+    request(app)
+      .delete('/api/users/1991')
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(404);
+        expect(res.body.error.code).to.be.equal(404);
+        expect(res.body.error.message).to.be.equal('User not found');
 
         done();
       });
