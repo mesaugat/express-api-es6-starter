492bebec5d02ee50ca64d3ce64ca18fe9fb085c9
Saugat Acharya <mesaugat@gmail.com>
2017-09-16 23:09:06 +0545

    Set UTC timezone

--- src/knexfile.js
+++ src/knexfile.js
@@ -12,7 +12,8 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_NAME : process.env.DB_NAME,
-    charset: 'utf8'
+    charset: 'utf8',
+    timezone: 'UTC'
   },
   migrations: {
     tableName: 'migrations'
