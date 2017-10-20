ac57e20440639d5fada44d6ea04e84c902b6acc0
Shreya Dahal <shreyadahal@gmail.com>
2017-10-20 21:47:12 +0545

    Support for writing API docs in yaml files (#12)
    * Add support for yaml/yml files for swagger doc
    
    * Move sample doc for user from jsdoc to yaml

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -21,7 +21,12 @@ const swaggerOptions = {
   // import swaggerDefinitions
   swaggerDefinition: swaggerDefinition,
   // path to the API docs
-  apis: [path.join(__dirname, '/../routes.js'), path.join(__dirname, '/../docs/*.js')]
+  apis: [
+    path.join(__dirname, '/../routes.js'),
+    path.join(__dirname, '/../docs/*.js'),
+    path.join(__dirname, '/../docs/*.yml'),
+    path.join(__dirname, '/../docs/*.yaml')
+  ]
 };
 
 /**
