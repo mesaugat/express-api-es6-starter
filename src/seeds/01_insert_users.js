8f7dea44e15e111d071ff20160b8686dfce9b8a8
Saugat Acharya <mesaugat@gmail.com>
2017-12-24 21:20:16 +0545

    Use array of objects to insert multiple items

--- src/seeds/01_insert_users.js
+++ src/seeds/01_insert_users.js
@@ -7,16 +7,21 @@
  */
 export function seed(knex, Promise) {
   // Deletes all existing entries
-  return knex('users')
+  return knex("users")
     .del()
     .then(() => {
       return Promise.all([
         // Inserts seed entries
-        knex('users').insert({
-          name: 'Saugat Acharya',
-          updated_at: new Date()
-        }),
-        knex('users').insert({ name: 'John Doe', updated_at: new Date() })
+        knex("users").insert([
+          {
+            name: "Saugat Acharya",
+            updated_at: new Date()
+          },
+          {
+            name: "John Doe",
+            updated_at: new Date()
+          }
+        ])
       ]);
     });
 }
