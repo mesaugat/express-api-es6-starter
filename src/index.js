1d0cb03830ecb3a7f7825dc6548b30a6e1723adf
Saugat Acharya <mesaugat@gmail.com>
2017-12-31 10:48:38 +0545

    Use process.env.PORT if process.env.APP_PORT is not set

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ import * as errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
-const APP_PORT = (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || '3000';
+const APP_PORT =
+  (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || process.env.PORT || '3000';
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
 
 app.set('port', APP_PORT);
