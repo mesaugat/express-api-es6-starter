982122b0a5ee67073980b43179d2c4580f0de41b
Saugat Acharya <mesaugat@gmail.com>
2018-07-24 00:57:41 +0545

    Remove bookshelf-camelcase

--- src/db.js
+++ src/db.js
@@ -8,6 +8,6 @@ import bookshelfJs from 'bookshelf';
 const knex = knexJs(knexConfig);
 const bookshelf = bookshelfJs(knex);
 
-bookshelf.plugin(['virtuals', 'pagination', 'visibility', 'bookshelf-camelcase']);
+bookshelf.plugin(['virtuals', 'pagination', 'visibility']);
 
 export default bookshelf;
