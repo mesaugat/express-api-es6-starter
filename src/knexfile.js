a74e385a30d4a81fa41e4df874de398769cd06f1
Saugat Acharya <mesaugat@gmail.com>
2018-10-22 15:22:38 +0545

    Use test credentials for test env

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,20 +1,35 @@
 require('@babel/register');
 require('dotenv').config({ path: __dirname + '/../.env' });
 
+// Default configuration for database connection
+let connection = {
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  charset: 'utf8',
+  timezone: 'UTC'
+};
+
+// For test environment
+if (process.env.NODE_ENV === 'test') {
+  connection = {
+    ...connection,
+    port: process.env.TEST_DB_PORT,
+    host: process.env.TEST_DB_HOST,
+    user: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME
+  };
+}
+
 /**
  * Database configuration.
  */
 module.exports = {
+  connection,
   client: process.env.DB_CLIENT,
-  connection: {
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.NODE_ENV === 'test' ? process.env.TEST_DB_NAME : process.env.DB_NAME,
-    charset: 'utf8',
-    timezone: 'UTC'
-  },
   migrations: {
     tableName: 'migrations',
     directory: './migrations',
