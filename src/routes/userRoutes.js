2b35200ef6f9fb4e0180990ecea3f21d23c89161
Subash Poudel <subash.poudel@seattleapplab.com>
2018-11-05 11:29:00 +0545

    Add user routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import * as userController from '../controllers/users';
+import { findUser, userValidator } from '../validators/userValidator';
+
+const router = Router();
+
+/**
+ * GET /api/users
+ */
+router.get('/', userController.fetchAll);
+
+/**
+ * GET /api/users/:id
+ */
+router.get('/:id', userController.fetchById);
+
+/**
+ * POST /api/users
+ */
+router.post('/', userValidator, userController.create);
+
+/**
+ * PUT /api/users/:id
+ */
+router.put('/:id', findUser, userValidator, userController.update);
+
+/**
+ * DELETE /api/users/:id
+ */
+router.delete('/:id', findUser, userController.deleteUser);
+
+export default router;
