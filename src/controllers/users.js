40d62be90d1f9604ccd28c33e4e10bd8b23a41ac
Subash Poudel <subash.poudel@seattleapplab.com>
2018-11-05 14:41:49 +0545

    Improve documentation

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,76 +2,71 @@ import HttpStatus from 'http-status-codes';
 import * as userService from '../services/userService';
 
 /**
- * Get all users
+ * Get all users.
+ *
  * @param {Object} req
  * @param {Object} res
  * @param {Function} next
  */
-export async function fetchAll(req, res, next) {
-  try {
-    const data = await userService.getAllUsers();
-    res.json({ data });
-  } catch (error) {
-    next(error);
-  }
+export function fetchAll(req, res, next) {
+  userService
+    .getAllUsers()
+    .then(data => res.json({ data }))
+    .catch(err => next(err));
 }
 
 /**
- * Get a user by its id
+ * Get a user by its id.
+ *
  * @param {Object} req
  * @param {Object} res
  * @param {Function} next
  */
-export async function fetchById(req, res, next) {
-  try {
-    const data = await userService.getUser(req.params.id);
-    res.json({ data });
-  } catch (error) {
-    next(error);
-  }
+export function fetchById(req, res, next) {
+  userService
+    .getUser(req.params.id)
+    .then(data => res.json({ data }))
+    .catch(err => next(err));
 }
 
 /**
- * Create a new user
+ * Create a new user.
+ *
  * @param {Object} req
  * @param {Object} res
  * @param {Function} next
  */
-export async function create(req, res, next) {
-  try {
-    const data = await userService.createUser(req.body);
-    res.status(HttpStatus.CREATED).json({ data });
-  } catch (error) {
-    next(error);
-  }
+export function create(req, res, next) {
+  userService
+    .createUser(req.body)
+    .then(data => res.status(HttpStatus.CREATED).json({ data }))
+    .catch(err => next(err));
 }
 
 /**
- * Update a user
+ * Update a user.
+ *
  * @param {Object} req
  * @param {Object} res
  * @param {next} next
  */
-export async function update(req, res, next) {
-  try {
-    const data = await userService.updateUser(req.params.id, req.body);
-    res.json({ data });
-  } catch (error) {
-    next(error);
-  }
+export function update(req, res, next) {
+  userService
+    .updateUser(req.params.id, req.body)
+    .then(data => res.json({ data }))
+    .catch(err => next(err));
 }
 
 /**
- * Delete a user
+ * Delete a user.
+ *
  * @param {Object} req
  * @param {Object} res
  * @param {next} next
  */
-export async function deleteUser(req, res, next) {
-  try {
-    const data = await userService.deleteUser(req.params.id);
-    res.status(HttpStatus.NO_CONTENT).json({ data });
-  } catch (error) {
-    next(error);
-  }
+export function deleteUser(req, res, next) {
+  userService
+    .deleteUser(req.params.id)
+    .then(data => res.status(HttpStatus.NO_CONTENT).json({ data }))
+    .catch(err => next(err));
 }
